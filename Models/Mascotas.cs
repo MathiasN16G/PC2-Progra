61c0d68d95b6ecfd872e53badc4090267a5d99c3
Mathias <mathias_nunez@usmp.pe>
2025-05-01 23:54:35 -0500

    terminandox14

--- Models/Mascotas.cs
+++ Models/Mascotas.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AdopcionMascotas.Modelos
 {
     public class Mascota
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nombre { get; set; }
+
+        [Range(0, 100)]
         public int Edad { get; set; }
+
+        [Required]
         public string Tipo { get; set; }
+
+        [Required]
         public string EstadoAdopcion { get; set; }
 
-        // Relación 1 a 1: una mascota solo puede estar en una adopción
-        public Adopcion? Adopcion { get; set; }
+       
     }
 }
