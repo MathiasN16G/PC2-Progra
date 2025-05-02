f5faa4de05d8048418c510bedbffbdd75d15846f
Mathias <mathias_nunez@usmp.pe>
2025-05-02 01:16:58 -0500

    acabe

--- obj/Debug/net9.0/AdopcionMascotas.AssemblyInfo.cs
+++ obj/Debug/net9.0/AdopcionMascotas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdopcionMascotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef904914920c9b78c9d9a577a9fbdedaabb6117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c0d68d95b6ecfd872e53badc4090267a5d99c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdopcionMascotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdopcionMascotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
